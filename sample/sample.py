14d7c32c8d0837c187d17d22bc0499549c838945
bhuvannarula <55291162+bhuvannarula@users.noreply.github.com>
2020-10-16 17:36:13 +0530

    Added sample

---
+++ sample/sample.py
@@ -0,0 +1,15 @@
+from math import pi
+
+'''
+This is sample code
+'''
+
+#Sample comment
+
+def samplefn():
+    print('Hello World')
+
+samplefn()
+templist = list(1,2,3,4)
+
+rounded = round(1100.345,2)
